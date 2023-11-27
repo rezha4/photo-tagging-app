6a96ba84bb950d7c92ddba202db3e7a0ae88746b
rezha4 <rezha.bahari@gmail.com>
2023-11-27 11:19:26 +0700

    bug: x y coordinate

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function printMousePos(event) {
-    console.log(`X: ${event.screenX}, Y: ${event.screenY}`)
+    console.log(`X: ${event.clientX}, Y: ${event.clientY}`)
+    const answers = document.querySelector(".answers");
+    answers.style.display = "block"
+    answers.style.left = `${event.clientX}px`;
+    answers.style.top = `${event.clientY}px`;
 }
 
 document.addEventListener("click", printMousePos)
