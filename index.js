605f93972b97112baa25db88053b952af6eb8a40
rezha4 <rezha.bahari@gmail.com>
2023-11-28 10:12:56 +0700

    need to work on win condition (take x y, compare to character's x y, if ==, win, else, alert that its wrong)

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
+let xCoordinate = 0;
+let yCoordinate = 0;
+
 function printMousePos(event) {
-    console.log(`X: ${event.clientX}, Y: ${event.clientY}`)
-    const answers = document.querySelector(".answers");
-    answers.style.display = "block"
-    answers.style.left = `${event.clientX}px`;
-    answers.style.top = `${event.clientY}px`;
+    xCoordinate = event.clientX;
+    yCoordinate = event.clientY;
+    console.log(`X: ${xCoordinate}, Y: ${yCoordinate}`)
+    const answerDialog = document.querySelector(".answers");
+    answerDialog.removeAttribute("hidden")
+    answerDialog.style.left = `${xCoordinate}px`;
+    answerDialog.style.top = `${yCoordinate}px`;
 }
 
-document.addEventListener("click", printMousePos)
+const image = document.querySelector("img");
+
+image.addEventListener("click", printMousePos);
+
+const answers = document.querySelectorAll("button");
+
+answers.forEach(button => {
+    button.addEventListener("mousedown", () => {
+        console.log(button.textContent);
+        if (button.textContent === "X") {
+            document.querySelector(".answers").setAttribute("hidden", "false")
+        } else {
+            
+        }
+    })
+})
