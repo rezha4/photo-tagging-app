1de65407a002978a050fac4f6d68c531c627565c
rezha4 <rezha.bahari@gmail.com>
2023-11-30 14:05:08 +0700

    added photos of characters to be searched

--- src/Title.jsx
+++ src/Title.jsx
@@ -1,11 +1,25 @@
+import wally from "./assets/wally.jpeg";
+import wilma from "./assets/wilma.png";
+import odlaw from "./assets/odlaw.png";
+
 export default function Title() {
   return (
     <>
-      <div className="text-center">
-        <h5 className="text-sm mt-2">Find these 3 characters to win the game!</h5>
-        <h4 className="text-sm">Waldo: Wizard: Wolde: </h4>
-        <div className="text-sm flex w-[300px] m-auto justify-between pb-3">
-          <p>Score: </p>
+      <div className="text-center mt-5">
+        <div className="flex flex-col justify-center items-center gap-2">
+          <div>
+            <p>Find these characters to win the game</p>
+          </div>
+          <div className="flex gap-2">
+            <p>Waldo: </p>
+            <img src={wally} alt=" " className="w-10 h-10" />
+            <p>Wilma: </p>
+            <img src={wilma} alt=" " className="w-10 h-10" />
+            <p>Odlaw: </p>
+            <img src={odlaw} alt=" " className="w-10 h-10" />
+          </div>
+        </div>
+        <div className="text-center mb-5">
           <p>Time:</p>
         </div>
       </div>
