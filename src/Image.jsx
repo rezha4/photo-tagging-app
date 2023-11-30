a14e086b6329fa5cdfe6ff427debc5afa75f68e0
rezha4 <rezha.bahari@gmail.com>
2023-11-30 16:07:12 +0700

    finished frontend overall -code needs work to be more efficient and concise -design needs transitions, color pallete and margins

--- src/Image.jsx
+++ src/Image.jsx
@@ -4,42 +4,95 @@ import { useState } from "react";
 
 function Image() {
   const [visibility, setVisibility] = useState(true);
-  const [xCoordinate, setXCoordinate] = useState(0);
-  const [yCoordinate, setYCoordinate] = useState(0);
+  const [answerCoordinate, setAnswerCoordinate] = useState({ x: 0, y: 0 });
   const [axis, setAxis] = useState({ x: 0, y: 0 });
-  const divStyle = {
+  const [answers, setAnswers] = useState([]);
+
+  const answerBoxStyle = {
     position: "absolute",
-    left: `${xCoordinate}px`,
-    top: `${yCoordinate}px`,
+    left: `${answerCoordinate.x}px`,
+    top: `${answerCoordinate.y}px`,
   };
+
+  function checkWin(array) {
+    if (
+      array.includes("WALDO") &&
+      array.includes("WILMA") &&
+      array.includes("ODLAW")
+    ) {
+      alert("YOU WIN");
+    }
+  }
+
   return (
     <>
-      <div style={divStyle} className="w-10" hidden={visibility}>
+      <div style={answerBoxStyle} className="w-10" hidden={visibility}>
         <div>
-          <p className="bg-blue-500 hover:bg-blue-400 w-20 m-1 text-xs text-center">
+          <p className="bg-white hover:bg-gray-100 border w-20 m-1 shadow-2xl text-xs text-center">
             Who's this?
           </p>
           <button
-            className="bg-blue-500 hover:bg-blue-400 w-20 m-1"
+            className="bg-white hover:bg-gray-100 border shadow-2xl w-20 m-1"
             onClick={() => {
-              if (axis.x > 22 && axis.x < 25 && axis.y > 8 && axis.y < 14) {
-                console.log("correct");
+              const newAnswer = [...answers];
+              if (
+                axis.x >= 41 &&
+                axis.x <= 44 &&
+                axis.y >= 16 &&
+                axis.y <= 21
+              ) {
+                newAnswer.push("WALDO");
+                if (!answers.includes("WALDO")) {
+                  setAnswers(newAnswer);
+                }
               }
               setVisibility(true);
+              checkWin(newAnswer);
             }}
           >
             Waldo
           </button>
-          <button className="bg-blue-500 hover:bg-blue-400 w-20 m-1">
-            Wizard
-          </button>
           <button
-            className="bg-blue-500 hover:bg-blue-400 w-20 m-1"
+            className="bg-white hover:bg-gray-100 border shadow-2xl w-20 m-1"
             onClick={() => {
-              console.log("Wolde");
+              const newAnswer = [...answers];
+              if (
+                axis.x >= 19 &&
+                axis.x <= 20 &&
+                axis.y >= 71 &&
+                axis.y <= 76
+              ) {
+                newAnswer.push("ODLAW");
+                if (!answers.includes("ODLAW")) {
+                  setAnswers(newAnswer);
+                }
+              }
+              setVisibility(true);
+              checkWin(newAnswer);
             }}
           >
-            Wolde
+            Odlaw
+          </button>
+          <button
+            className="bg-white hover:bg-gray-100 border shadow-2xl w-20 m-1"
+            onClick={() => {
+              const newAnswer = [...answers];
+              if (
+                axis.x >= 29 &&
+                axis.x <= 30 &&
+                axis.y >= 72 &&
+                axis.y <= 77
+              ) {
+                newAnswer.push("WILMA");
+                if (!answers.includes("WILMA")) {
+                  setAnswers(newAnswer);
+                }
+              }
+              setVisibility(true);
+              checkWin(newAnswer);
+            }}
+          >
+            Wilma
           </button>
           {/* <button
             className="bg-red-500 hover:bg-red-400 w-20 m-1"
@@ -56,26 +109,35 @@ function Image() {
         alt=" "
         className="m-auto w-auto h-[500px] max-w-[800px]"
         onClick={(e) => {
+          //show/hide on image click
+          visibility ? setVisibility(false) : setVisibility(true);
+
+          //show answer box beneath cursor
+          const newCoordinate = { ...answerCoordinate };
+          newCoordinate.x = e.clientX - 50;
+          newCoordinate.y = e.clientY;
+          setAnswerCoordinate(newCoordinate);
+
+          //get normalized answer (percentage)
           const rect = e.currentTarget.getBoundingClientRect(); // Get the dimensions of the container
           const containerWidth = rect.width;
           const containerHeight = rect.height;
 
-          setXCoordinate(e.clientX - 50);
-          setYCoordinate(e.clientY);
-          visibility ? setVisibility(false) : setVisibility(true);
-
-          const percentX = Math.round(
+          const x = Math.round(
             ((e.clientX - rect.left) / containerWidth) * 100
           );
-          const percentY = Math.round(
+          const y = Math.round(
             ((e.clientY - rect.top) / containerHeight) * 100
           );
 
           const newAxis = { ...axis };
-          newAxis.x = percentX;
-          newAxis.y = percentY;
+          newAxis.x = x;
+          newAxis.y = y;
           setAxis(newAxis);
-          console.log(`percentX: ${axis.x} percentY: ${axis.y}`);
+
+          //for catching the coordinates to set win condition
+          //console.log(`X:${axis.x}, Y:${axis.y}`);
+          console.log(answers);
         }}
       />
     </>
